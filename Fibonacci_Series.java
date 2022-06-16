c622ee24dccad26d33545a4cf9faf7d150f3501d
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-06-16 18:35:26 +0530

    Add files via upload

---
+++ Fibonacci_Series.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Fibonacci_Series
+{
+    public static void main(String[] args)
+    {
+        fibo();
+    }
+
+    public static void fibo()
+    {
+        Scanner sc = new Scanner(System.in);
+        int n1, n2,n3,count;
+        System.out.print("Please enter the two value for Fibonacci Series : ");
+        n1 = sc.nextInt();
+        n2 = sc.nextInt();
+        System.out.println(" Your starting value is : "+n1+"   Your Ending value is : "+n2);
+        System.out.println("Please enter the Counting value : ");
+        count = sc.nextInt();
+        for(int i=2 ; i<count; ++i)
+        {
+            n3 = n1+n2;
+            System.out.print(" "+n3);
+            n1 = n2;
+            n2 = n3;
+        }
+
+    }
+}
---
+++ Palidorm_No.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Palidorm_No
+{
+    public static void main(String[] args)
+    {
+        palidorm();
+    }
+
+    public static void palidorm()
+    {
+        Scanner sc = new Scanner(System.in);
+       
+       int n, r,sum=0,temp;    
+       
+       System.out.print("Please the no : ");
+       n= sc.nextInt();
+  
+        temp=n;    
+        while(n>0)
+        {    
+            r = n%10;  
+            sum = (sum*10)+r;    
+            n = n/10;    
+        }
+
+        if(temp==sum)    
+        System.out.println("Palindrome number :  "+sum);    
+        else    
+        System.out.println("Not palindrome : ");    
+        }  
+
+}
