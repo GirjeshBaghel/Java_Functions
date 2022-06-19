42ff6c7337d45647365f7863e7c01b6a9666827f
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-06-19 23:19:30 +0530

    Update Swap.java

--- Swap.java
+++ Swap.java
@@ -1,11 +1,8 @@
+//WAP to swap a two values without taking a third no
 import java.util.*;
 class Swap
 {
-    public static void main(String[] args)
-    {
-        accept();
-    }
-
+    // accept method accepting two no from user
     public static void accept()
     {
         int a,b;
@@ -17,10 +14,16 @@ class Swap
         System.out.println("Old value of a : "+a);
         System.out.println("Old value of b : "+b);
 
+        // logical of swaping value
         a = a+b;
         b = a-b;
         a = a-b;
         System.out.println("New value of a : "+a);
         System.out.println("New value of b : "+b);
+    }
+    
+    public static void main(String[] args)
+    {
+        accept();
     }
 }
